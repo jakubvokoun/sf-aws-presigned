74f1a6ef3758c99e3390359da27437843f6b5499
Jakub Vokoun <jakub.vokoun@gmail.com>
2020-12-14 14:50:27 +0100

    Presigned POST example added
    Signed-off-by: Jakub Vokoun <jakub.vokoun@gmail.com>

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Aws\S3\S3Client;
+use Aws\S3\PostObjectV4;
 
 class ApiController extends AbstractController
 {
@@ -18,5 +19,33 @@ class ApiController extends AbstractController
         $request = $s3Client->createPresignedRequest($cmd, '+20 minutes');
         $presignedUrl = (string) $request->getUri();
         return $this->json(['presignedUrl' => $presignedUrl]);
+    }
+
+    public function getPresignedUploadUrl(S3Client $s3Client, Request $request): Response
+    {
+        $bucket = $this->getParameter('app.s3_bucket');
+        $formInputs = ['acl' => 'public-read'];
+        $options = [
+            ['acl' => 'public-read'],
+            ['bucket' => $bucket],
+            ['starts-with', '$key', 'test/'],
+        ];
+        $expires = '+2 hours';
+
+        $postObject = new PostObjectV4(
+            $s3Client,
+            $bucket,
+            $formInputs,
+            $options,
+            $expires
+        );
+
+        $formAttributes = $postObject->getFormAttributes();
+        $formInputs = $postObject->getFormInputs();
+
+        return $this->json([
+            'formAttributes' => $formAttributes,
+            'formInputs' => $formInputs,
+        ]);
     }
 }
